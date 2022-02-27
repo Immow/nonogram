13cfa37c9b2545c7fa476ccfe334854a973f53b1
Immow <k.schippers@gmail.com>
2022-02-27 03:22:50 +0100

    step one of solver done, column and row

--- solver.lua
+++ solver.lua
@@ -9,7 +9,7 @@ local boardDimensions = require("board_dimensions")
 local Solver = {}
 Solver.results = {}
 
-function Solver:markCells(count, i)
+function Solver:markRow(count, i)
 	print("Solving row: "..i)
 	local index = #boardDimensions.resultLeft[i]
 	local chunkIndex = 1
@@ -50,6 +50,49 @@ function Solver:markCells(count, i)
 	end
 end
 
+function Solver:markColumn(count, i)
+	print("Solving column: "..i)
+	local index = #boardDimensions.resultTop[i]
+	local chunkIndex = 1
+	local difference = #problems[s.problem] - count
+	if difference == 0 then
+		local number = boardDimensions.resultTop[i][index] -- select the left most number
+		for j = 1, #boardCellsMain.boardCells do
+			if chunkIndex <= number then
+				boardCellsMain.boardCells[j][i]:markCellSolver()
+			end
+			
+			if chunkIndex > number then
+				index = index - 1
+				number = boardDimensions.resultTop[i][index]
+				chunkIndex = 1
+				boardCellsMain.boardCells[j][i]:crossCell()
+				print("itteration: "..j.." marking line")
+			else
+				chunkIndex = chunkIndex + 1
+			end
+		end
+	else
+		local number = boardDimensions.resultTop[i][index] -- select the left most number
+		for j = 1, #boardCellsMain.boardCells do
+
+			if chunkIndex > difference and number - chunkIndex >= 0 and number > difference then
+				print("itteration: "..j.." marking cell: "..boardCellsMain.boardCells[j][i].position[1]..", "..boardCellsMain.boardCells[j][i].position[2])
+				boardCellsMain.boardCells[j][i]:markCellSolver()
+			end
+
+			if chunkIndex > number + 1 and index > 1 then
+				index = index - 1
+				number = boardDimensions.resultTop[i][index]
+				chunkIndex = 1
+			end
+			chunkIndex = chunkIndex + 1
+		end
+	end
+end
+
+
+
 function Solver.countNumbers()
 	local count = 0
 	for i = 1, #boardDimensions.resultLeft do
@@ -59,7 +102,18 @@ function Solver.countNumbers()
 				if j > 1 then count = count + 1 end
 			end
 		end
-		Solver:markCells(count, i)
+		Solver:markRow(count, i)
+		count = 0
+	end
+
+	for i = 1, #boardDimensions.resultTop do
+		for j = 1, #boardDimensions.resultTop[i] do
+			if boardDimensions.resultTop[i][j] then
+				count = count + boardDimensions.resultTop[i][j]
+				if j > 1 then count = count + 1 end
+			end
+		end
+		Solver:markColumn(count, i)
 		count = 0
 	end
 end
