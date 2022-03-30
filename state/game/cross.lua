d87de70a98cc9585d9feecf9a8cb1f9914000b49
Immow <k.schippers@gmail.com>
2022-03-30 14:05:49 +0200

    - copyright update - more refactoring

--- state/game/cross.lua
+++ state/game/cross.lua
@@ -2,7 +2,7 @@ local s = require("settings")
 
 local Cross = {}
 
-function Cross:newCross(x, y)
+function Cross.newCross(x, y)
 	local offset = 5
 	local topLefttoBottomRight = {x + offset,y + offset, x - offset + s.cellSize, y - offset + s.cellSize}
 	local topRighttoBottomLeft = {x - offset + s.cellSize, y + offset, x + offset, y - offset + s.cellSize}
